1223162fe897b8dc9fccc04a8b7fa867f4b7fa8e
Rupesh Budhathoki <>
2021-06-06 07:35:04 +0000

    Reqrote the code

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
-import neverSleep
-neverSleep.awake('https://Roli-Roli.roopaish.repl.co')
+### Keep the server running all the time
+# import neverSleep
+
+# neverSleep.awake('https://Roli-Roli.roopaish.repl.co')
 
 import discord, os, requests, json, random
+from discord.ext import commands
 
+### Using replit database
 from replit import db
 
+### Getting token from environment
 token = os.environ['TOKEN']
 
-# to sense sad word and give uplifting_words
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
-starter_uplifting_words = ["Cheer up!", "Hang in there!"]
+### Extensions
+
+### Connection to discord
+client = commands.Bot(command_prefix=".")
 
 
-# generate random jokes
+### Register event when bot is ready
+@client.event
+async def on_ready():
+    print(f"You are Logged in as {client.user}")
+
+
+### Bot commands with functions
+
+# Greetings
+@client.command(name = 'hi', aliases = ['hey', 'hello'])
+async def hi(ctx):
+    await ctx.send('>>> ```Hey there!\
+                \nI am Roli.\
+                \nI am here to make you laugh and play music for you.```')
+
+# Check ping
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'**Pong!** {round(client.latency * 1000)}ms')
+
+# Clear messages
+@client.command()
+# @commands.has_permissions(manage_message = True)
+# @commands.has_role("admin")
+async def clear(ctx, amount = 1):
+    await ctx.channel.purge(limit = amount)
+
+# Generate random jokes
 def random_joke():
     response = requests.get(
         'https://official-joke-api.appspot.com/random_joke')
@@ -20,17 +53,44 @@ def random_joke():
     joke = json_data['setup'] + '\n\n' + json_data['punchline']
     return joke
 
+@client.command(aliases=['rj'])
+async def random_jokes(ctx):
+    await ctx.send(random_joke())
 
-# genrate programming jokes
+
+# Generate programming jokes
 def programming_joke():
     response = requests.get(
-        'https://official-joke-api.appspot.com/jokes/programming/random')
+            'https://official-joke-api.appspot.com/jokes/programming/random')
     json_data = json.loads(response.text)
     joke = json_data[0]['setup'] + '\n\n' + json_data[0]['punchline']
     return joke
 
+@client.command(aliases=['pj'])
+async def programming_jokes(ctx):
+    await ctx.send(programming_joke())
 
-# update uplifting_words
+
+# Respond to sad words or not
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+starter_uplifting_words = ["Cheer up!", "Hang in there!"]
+
+if 'responding' not in db.keys():
+    db['responding'] = True
+
+
+# Toggle responding to sad message
+@client.command(aliases=['respond'])
+async def responding(ctx, value):
+    if value.lower() == "true":
+        db['responding'] = True
+        await ctx.send('Responding is on.')
+    else:
+        db['responding'] = False
+        await ctx.send('Responding is off.')
+
+
+# Add new uplifting words
 def update_uplifting_words(msg):
     if 'uplifting_words' in db.keys():
         uplifting_words = db['uplifting_words']
@@ -40,7 +100,13 @@ def update_uplifting_words(msg):
         db['uplifting_words'] = [msg]
 
 
-# delete uplifting_words
+@client.command()
+async def new(ctx, *, words):
+    update_uplifting_words(words)
+    await ctx.send('New uplifting words added!')
+
+
+# Delete uplifting_words
 def delete_uplifting_words(index):
     uplifting_words = db['uplifting_words']
     if (len(uplifting_words) > index):
@@ -48,52 +114,36 @@ def delete_uplifting_words(index):
         db['uplifting_words'] = uplifting_words
 
 
-# respond to word or not
-if 'responding' not in db.keys():
-    db['responding'] = True
+@client.command(aliases = ['del'])
+async def delete_words(ctx, index):
+    deleted_words = ''
+    if 'uplifting_words' in db.keys():
+        deleted_words = db['uplifting_words'][int(index) - 1]
+        delete_uplifting_words(int(index) - 1)
 
-# Connection to discord
-client = discord.Client()
-
-
-# register event when bot is ready
-@client.event
-async def on_ready():
-    print(f"You are Logged in as {client.user}")
+    await ctx.send(f'\'{deleted_words}\' deleted.')
 
 
-# sensing message
+# list all uplifting words
+@client.command('list')
+async def list_words(ctx):
+    uplifting_words = []
+    if 'uplifting_words' in db.keys():
+        uplifting_words = db['uplifting_words']
+    await ctx.send(uplifting_words.value)
+
+
+# Sensing messages in server
 @client.event
 async def on_message(message):
+    # processing commands in order to use commands decorator
+    await client.process_commands(message)
     # checking if the msg is by bot itself
     if message.author == client.user:
         return
 
     msg = message.content
 
-    # helper/intro command
-    if msg.startswith("$h"):
-        await message.channel.send(
-            'Hey There! I am roli. \
-            \n I am here to assist you to bring laughter in your miserable life.\
-            \n\n $j -> random joke\
-            \n $p -> programming joke\
-            \n $responding -> set if the bot will respond to sad message or not\
-            \n $list -> list all uplifting words\
-            \n $new msg -> add new uplifting message\
-            \n $del index -> delete uplifting message\
-            \n\n Source code -> <https://replit.com/@Roopaish/Roli-Roli>\
-            \n Github -> <https://github.com/Roopaish/Roli-Roli>\
-            \n Install Link -> <https://discord.com/api/oauth2/authorize?client_id=849639569733058590&permissions=2151148096&scope=bot>'
-        )
-
-    # get jokes with commands
-    if msg.startswith('$j'):
-        await message.channel.send(random_joke())
-    if msg.startswith('$p'):
-        await message.channel.send(programming_joke())
-
-    # sensing sad words
     if db['responding']:
         options = starter_uplifting_words
         if 'uplifting_words' in db.keys():
@@ -104,38 +154,6 @@ async def on_message(message):
                 f'{random.choice(options)}\nI have something to make you laugh\n\n{random_joke()}'
             )
 
-    if msg.startswith('$responding'):
-        value = msg.split('$responding ', 1)[1]
-
-        if value.lower() == "true":
-            db['responding'] = True
-            await message.channel.send('Responding is on.')
-        else:
-            db['responding'] = False
-            await message.channel.send('Responding is off.')
-
-    # adding new uplifting words
-    if msg.startswith('$new'):
-        uplifting_msg = msg.split("$new ", 1)[1]
-        update_uplifting_words(uplifting_msg)
-        await message.channel.send('New uplifting word added!')
-
-    # deleting new uplifting words
-    if msg.startswith('$del'):
-        uplifting_words = []
-        if 'uplifting_words' in db.keys():
-            index = int(msg.split('$del', 1)[1])
-            delete_uplifting_words(index)
-            uplifting_words = db['uplifting_words']
-
-        await message.channel.send(uplifting_words.value)
-
-    # listing uplifting words
-    if msg.startswith('$list'):
-        uplifting_words = []
-        if 'uplifting_words' in db.keys():
-            uplifting_words = db['uplifting_words']
-        await message.channel.send(uplifting_words.value)
-
 
 client.run(token)
+
